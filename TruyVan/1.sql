c7f40ff14c98a1a784c23da10004714077d30c07
DangSonPCB <143177693+DangSonPCB@users.noreply.github.com>
2026-03-27 02:31:49 +0700

    themdulieu.sql

---
+++ TruyVan/1.sql
@@ -0,0 +1,7 @@
+--Thêm dữ liệu vào bảng khách 
+INSERT INTO KHACHHANG (MaKH, TenKH, SDTKH, NgaySinhKH, GioiTinh, CCCDKH)
+
+VALUES 
+  ('KH03', N'Phạm Minh Hoàng', '0933444555', '1985-12-01', N'Nam', '001085123987');
+  ('KH04', N'Phạm Minh Hoàng', '0933444555', '1985-12-01', N'Nam', '001085123987');
+SELECT * FROM KHACHANG;
